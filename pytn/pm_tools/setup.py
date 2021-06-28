3e49412b4e820b6bf5000b4bd81b9bea75514091
Prem Vijay V <premvpv18@gmail.com>
2021-06-28 17:09:24 +0530

    Update setup.py
    seperate modules

--- pytn/pm_tools/setup.py
+++ pytn/pm_tools/setup.py
@@ -4,12 +4,17 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 ext_modules = [
     Pybind11Extension(
-        "pm_tools",
-        sorted(glob("src/*.cpp")),
+        "particles_to_grid",
+        ["src/grid_assign_pybind11.cpp"]),
+    ),
+    Pybind11Extension(
+        "select_particles",
+        ["src/particle_selection_pybind11.cpp"]),
     ),
 ]
 
 setup(
+    name="pm_tools",
     cmdclass={"build_ext": build_ext},
     ext_modules=ext_modules
 )
