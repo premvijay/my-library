a50bd8d304aab422eb56ea0e59dbf9dc264bbc1f
cprem <premv@iucaa.in>
2022-06-12 19:06:16 +0530

    added a function needed for computing halo centric coords in period bc

--- pytn/pm_tools/__init__.py
+++ pytn/pm_tools/__init__.py
@@ -1,3 +1,8 @@
 from .density_assignment import assign_density, assign_density_sph
 from .projection_3D_to_2D import average_2D_slice as project_to_slice
 from .particles import Region, Transform
+
+def unperiod(pos,lenscl,box_size):
+    pos[pos>lenscl]-=box_size
+    pos[pos<-lenscl]+=box_size
+    return pos
