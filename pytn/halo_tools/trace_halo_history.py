606ef71893e58c42bd99dab554cbe9a851cb00b7
cprem <premv@iucaa.in>
2024-02-19 21:21:34 +0530

    extended crawl_illustris with efficient scalability

--- pytn/halo_tools/trace_halo_history.py
+++ pytn/halo_tools/trace_halo_history.py
@@ -52,27 +52,35 @@ def mmp_branch(halosfile, treesdir, upto=1):
 
 
 
-def crawl_illustris(halo_id, simname = 'TNG100-1', snapnum_start=98, snapnum_trace=49):
+def crawl_illustris(halo_id, simname = 'TNG100-1', snapnum_start=98, snapnum_trace=49, all_across=False, return_R200c=True):
     crawl_len = snapnum_start-snapnum_trace
     with h5py.File(os.environ['SCRATCH'] + f'/download/IllTNG/{simname}/simulation.hdf5', mode='r') as simfile:
-        subhalo_id = simfile[f'/Groups/{snapnum_start:d}/Group/GroupFirstSub'][:int(halo_id.max()+1)][halo_id]
-        sublink_ind = simfile['Offsets/98/Subhalo/SubLink']['RowNum'][:int(subhalo_id.max()+1)][subhalo_id]
-        sublink_prelod_len = int(sublink_ind.max()+crawl_len+1)
+        subhalo_id = simfile[f'/Groups/{snapnum_start:d}/Group/GroupFirstSub'][:int(np.max(halo_id)+1)][halo_id]
+        sublink_ind = simfile['Offsets/98/Subhalo/SubLink']['RowNum'][:int(np.max(subhalo_id)+1)][subhalo_id]
+        sublink_prelod_len = int(np.max(sublink_ind)+crawl_len+1)
         subln_preload_snapnum = simfile['Trees/SubLink']['SnapNum'][:sublink_prelod_len]
         # subln_preload_sbhlID = simfile['Trees/SubLink']['SubfindID'][:sublink_prelod_len]
         subln_preload_hosthlID = simfile['Trees/SubLink']['SubhaloGrNr'][:sublink_prelod_len]
-        subln_preload_hosthlR200c = simfile['Trees/SubLink']['Group_R_Crit200'][:sublink_prelod_len]
-
-        # subhalo_id_traced = subln_preload_sbhlID[sublink_ind+crawl_len]
-        halo_id_traced = subln_preload_hosthlID[sublink_ind+crawl_len]
-        halo_R200c_traced = subln_preload_hosthlR200c[sublink_ind+crawl_len]
 
         filter_matchhals_ind = np.where((sublink_ind!=-1) & (subln_preload_snapnum[sublink_ind+crawl_len]==snapnum_trace))
+
+        # subhalo_id_traced = subln_preload_sbhlID[sublink_ind+crawl_len]
+        if all_across:
+            halo_id_traced = subln_preload_hosthlID[np.linspace(sublink_ind,sublink_ind+crawl_len, crawl_len+1, dtype='int')].T
+        else:
+            halo_id_traced = subln_preload_hosthlID[sublink_ind+crawl_len]
+        
+        res_list = [halo_id_traced[filter_matchhals_ind], filter_matchhals_ind]
+        if return_R200c:
+            subln_preload_hosthlR200c = simfile['Trees/SubLink']['Group_R_Crit200'][:sublink_prelod_len]
+            halo_R200c_traced = subln_preload_hosthlR200c[sublink_ind+crawl_len]
+            res_list.append(halo_R200c_traced[filter_matchhals_ind])
+
         # print(filter_matchhals_ind[0].shape)
         # halo_id_start = halo_id[filter_matchhals_ind]
         # halo_id_traced = simfile[f'/Groups/{snapnum_trace:d}/Subhalo/SubhaloGrNr'][:int(subhalo_id_traced.max()+1)][subhalo_id_traced[filter_matchhals_ind]]
     # simfile.close()
-    return halo_id_traced[filter_matchhals_ind], filter_matchhals_ind, halo_R200c_traced[filter_matchhals_ind]
+    return res_list
 
 
 if __name__=='__main__':
